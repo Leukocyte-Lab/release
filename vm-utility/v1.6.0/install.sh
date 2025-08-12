c59157dcf18db5ac1443c97d7ee1bb14164b6c61
Aries0d0f <Aries0d0f@users.noreply.github.com>
2025-08-12 10:18:36 +0000

    Release from ref: v1.6.0@b598889f2420b2617656ad12db98cd7e14528563

---
+++ vm-utility/v1.6.0/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+DEFAULT_UTILITY_URL="https://release.lkc-lab.com"
+DEFAULT_UTILITY_VERSION="latest"
+
+UTILITY_ARCHIVE="utility.tgz"
+UTILITY_VERSION="${VERSION:-$DEFAULT_UTILITY_VERSION}"
+UTILITY_URL="${URL:-$DEFAULT_UTILITY_URL}"
+UTILITY_DOWNLOAD_LINK="$UTILITY_URL/vm-utility/$UTILITY_VERSION/$UTILITY_ARCHIVE"
+
+echo "Downloading VM Utility version: $UTILITY_VERSION..."
+echo "Download link: $UTILITY_DOWNLOAD_LINK"
+
+if wget -q --show-progress -O $UTILITY_ARCHIVE "$UTILITY_DOWNLOAD_LINK"; then
+  echo "VM Utility downloaded successfully!"
+else
+  echo "Failed to download VM Utility version: $UTILITY_VERSION"
+  exit 1
+fi
+
+echo "Installing VM Utility..."
+mkdir -p "$HOME/utility"
+tar zxvf utility.tgz -C "$HOME/utility"
+rm -f utility.tgz
+
+echo "Setup VM Utility..."
+echo "source \"\$HOME/utility/shell.sh\"" > "$HOME/.bash_login"
+
+echo "VM Utility has been installed successfully!"
